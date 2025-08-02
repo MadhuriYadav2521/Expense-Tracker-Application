8f85302badfdd31abab422905e841eb6ea2d7d5d
Yadav Madhuri <madhuri.y.0932@gmail.com>
2025-08-02 14:12:23 +0530

    url changed

--- frontend/src/services/transactionService.js
+++ frontend/src/services/transactionService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/transactions";
+const BASE_URL = "https://expense-tracker-application-s09e.onrender.com/transactions";
 
 export const AddTransactionAxios = async (data) => {
     try {
